8d19bbaa9c2b17da0c465c6d82ea47779fbff769
Nathan Cho <nathanch092@gmail.com>
2017-07-19 09:50:59 -0700

    updated one prediction

--- src1/one_prediction.py
+++ src1/one_prediction.py
@@ -2,8 +2,8 @@ import pickle
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
-import src1.feature_extraction as feature
-from src1.image_resize import image_resize
+import feature_extraction as feature
+from image_resize import image_resize
 
 """
 This script will take in a image path, and classify the image based on the
@@ -36,7 +36,7 @@ def predict_one(image_path):
     """
 
     re_size = image_resize([image_path])
-    predicted_image_path = 'project_photos/one_prediction/predicted.jpg'
+    predicted_image_path = 'one_prediction/predicted.jpg'
     feature_vector = feature.extraction([predicted_image_path])
     model_path = 'pickle_files/svm_model.pkl'
     with open(model_path, 'rb') as s:
@@ -47,11 +47,11 @@ def predict_one(image_path):
 
     print('\nYour prediction...')
     if prediction_class == 'kickflip':
-        print(prediction_class, prediction[0])
-        image_show(image_path)
+        return prediction_class[0] ,prediction[0][0]
+        # image_show(image_path)
     else:
-        print(prediction_class, prediction[0])
-        image_show(image_path)
+        return prediction_class[0] ,prediction[0][1]
+        # image_show(image_path)
 
 # Photo paths that I have tested on.
 
